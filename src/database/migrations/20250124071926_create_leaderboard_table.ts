e0b17ed73bdc62087c33aaee878bf4d61377d6dd
Emmanuel Abraham <sundayemmanuelabraham@gmail.com>
2025-01-26 18:16:26 +0100

    chore: added

--- src/database/migrations/20250124071926_create_leaderboard_table.ts
+++ src/database/migrations/20250124071926_create_leaderboard_table.ts
@@ -2,15 +2,15 @@ import { Knex } from "knex";
 import { DB_TABLES } from "../../shared/enums/db-tables.enum";
 
 exports.up = function (knex) {
-    return knex.schema.createTable(DB_TABLES.LEADERBOARD, (table: Knex.TableBuilder) => {
-      table.integer("position").notNullable();
-      table.string("team").primary();
-      table.integer("total_score").notNullable();
-      table.timestamps(true, true);
-    });
-  };
+  return knex.schema.createTable(DB_TABLES.LEADERBOARD, (table: Knex.TableBuilder) => {
+    table.integer("position").notNullable();
+    table.string("team").primary();
+    table.integer("total_score").notNullable();
+    table.timestamps(true, true);
+  });
+};
   
-  exports.down = function (knex) {
-    return knex.schema.dropTable(DB_TABLES.LEADERBOARD);
-  };
+exports.down = function (knex) {
+  return knex.schema.dropTable(DB_TABLES.LEADERBOARD);
+};
   
